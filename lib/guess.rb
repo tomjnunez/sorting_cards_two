98a7cdc531ca22daf78f80eb980134b5010ab29c
Tom Nunez <tomjnunez@gmail.com>
2018-08-21 13:47:09 -0600

    finished iteration one

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,3 +1,25 @@
+require './lib/card'
 class Guess
-  
+  attr_reader :response, :card
+
+  def initialize(response,card)
+    @response = response
+    @card = card
+  end
+
+  def correct?
+    if @response == "#{card.value} of #{card.suit}"
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if correct? == true
+      "Correct!"
+    else
+      "Sorry, Incorrect!"
+    end
+  end
 end
